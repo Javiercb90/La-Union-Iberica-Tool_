e30b077f1e69132643674d5b822ba66da657ab10
Javier Carrera <javiercb90@outlook.es>
2025-12-29 21:01:35 +0100

    Control Alianza sin posibilidad de fastidiar el código por otros

--- js/unionIberica/control_paginas.js
+++ js/unionIberica/control_paginas.js
@@ -1,11 +1,39 @@
+  function isLaUnionIberica() {
+  const rows = document.querySelectorAll("table tr");
+
+    for (const row of rows) {
+      const tds = row.querySelectorAll("td");
+      if (tds.length < 2) continue;
+
+      if (
+        tds[0].textContent.trim().toUpperCase() === "NOMBRE" &&
+        tds[1].textContent.trim().toUpperCase() === "LA UNION IBERICA"
+      ) {
+        return true;
+      }
+    }
+  return false;
+  }
+
+  
   function isAlliancePage() {
-    const params = new URLSearchParams(window.location.search);
-    return params.get("page") === "alliance";
+  const params = new URLSearchParams(window.location.search);
+  if (params.get("page") !== "alliance") return false;
+
+  return isLaUnionIberica();
   }
 
   function isAllianceMemberList() {
-    const params = new URLSearchParams(window.location.search);
-    return params.get("page") === "alliance" && params.get("mode") === "memberList";
+  const params = new URLSearchParams(window.location.search);
+
+    if (
+      params.get("page") !== "alliance" ||
+      params.get("mode") !== "memberList"
+    ) {
+      return false;
+    }
+
+  return isLaUnionIberica();
   }
 
   function isEmpirePage() {
